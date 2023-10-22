216197436af29272331a7f79ca21f6f89d91d171
SergioRon2 <rondonjr2005@gmail.com>
2023-10-21 23:35:30 -0500

    21/10/2023

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,3 +8,10 @@ formulario.addEventListener('mouseover', function() {
 formulario.addEventListener('mouseout', function() {
   body.style.backdropFilter = 'brightness(.7)';
 });
+
+
+window.addEventListener("load", function() {
+  // Cuando la página se carga completamente, oculta la animación de carga.
+  document.querySelector(".loader").style.display = "none";
+  document.querySelector(".content").style.display = "block";
+});
